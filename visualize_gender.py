d181fdb10afdc5e96cb22c4b4f6e7444ca956b2c
HirushaR <hirusharandunu11@gmail.com>
2020-07-06 13:50:08 +0530

    rich or poor servived chart

--- visualize_gender.py
+++ visualize_gender.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 female_color = "#FA0000"
 
 df = pd.read_csv('data/train.csv')
-fig = plt.figure(figsize=(18,6))
+fig = plt.figure(figsize=(18,8))
 
 plt.subplot2grid((3,4),(0,0))
 df.Survived.value_counts(normalize=True).plot(kind='bar',alpha=0.5)
@@ -21,5 +21,27 @@ plt.title('Women Survived')
 plt.subplot2grid((3,4),(0,3))
 df.Sex[df.Survived == 1].value_counts(normalize=True).plot(kind='bar',alpha=0.5, color=female_color)
 plt.title('Sex of Survived')
+
+plt.subplot2grid((3,4),(1,0),colspan=4)
+for x in [1,2,3]:
+    df.Survived[df.Pclass == x].plot(kind='kde')
+plt.title('Class wrt Survived')
+plt.legend(("1st", "2nd", "3rd"))
+
+plt.subplot2grid((3,4),(2,0))
+df.Survived[(df.Sex == "male")&(df.Pclass == 1)].value_counts(normalize=True).plot(kind='bar',alpha=0.5)
+plt.title('Rich Man Survived')
+
+plt.subplot2grid((3,4),(2,1))
+df.Survived[(df.Sex == "male")&(df.Pclass == 3)].value_counts(normalize=True).plot(kind='bar',alpha=0.5)
+plt.title('Poor Man Survived')
+
+plt.subplot2grid((3,4),(2,2))
+df.Survived[(df.Sex == "female")&(df.Pclass == 1)].value_counts(normalize=True).plot(kind='bar',alpha=0.5 , color=female_color)
+plt.title('Rich Women Survived')
+
+plt.subplot2grid((3,4),(2,3))
+df.Survived[(df.Sex == "female")&(df.Pclass == 3)].value_counts(normalize=True).plot(kind='bar',alpha=0.5 , color=female_color)
+plt.title('Poor Women Survived')
 
 plt.show()
