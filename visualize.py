9feb8141c145bd7ddef32ea90be879e092906222
HirushaR <hirusharandunu11@gmail.com>
2020-07-06 10:30:34 +0530

    passenger age with class chart

--- visualize.py
+++ visualize.py
@@ -14,6 +14,13 @@ plt.title('Age wrt Survived')
 
 plt.subplot2grid((2,3),(0,2))
 df.Pclass.value_counts(normalize=True).plot(kind='bar',alpha=0.5)
-plt.title('PClass')
+plt.title('Class')
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+for x in [1,2,3]:
+    df.Age[df.Pclass == x].plot(kind='kde')
+plt.title('class wrt Age')
+plt.legend(("1st", "2nd", "3rd"))
+
 
 plt.show()
