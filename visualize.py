ac403a2f757ae40323833d3226e86bf47fe7bc97
HirushaR <hirusharandunu11@gmail.com>
2020-07-06 10:25:00 +0530

    age with survived chart

--- visualize.py
+++ visualize.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data/train.csv')
 fig = plt.figure(figsize=(18,6))
 
-df.Survived.value_counts().plot(kind='bar',alpha=0.5)
+plt.subplot2grid((2,3),(0,0))
+df.Survived.value_counts(normalize=True).plot(kind='bar',alpha=0.5)
+plt.title('Survived')
 
+plt.subplot2grid((2,3),(0,1))
+plt.scatter(df.Survived, df.Age, alpha=0.1)
+plt.title('Age wrt Survived')
 plt.show()
