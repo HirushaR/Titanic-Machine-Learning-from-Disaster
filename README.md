d82f6ba4a2e0a3bec15d6b5e6a52193a3540e673
HirushaR <hirusharandunu11@gmail.com>
2020-07-07 10:36:53 +0530

    logisteic regression 01

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Titanic-Machine-Learning-from-Disaster
+
+0.7912457912457912
+0.8013468013468014
--- predict_logistic_regression.py
+++ predict_logistic_regression.py
@@ -6,7 +6,7 @@ train = pd.read_csv('data/train.csv')
 utils.clean_data(train)
 
 target = train["Survived"].values
-features = train[["Pclass","Age","Sex","SibSp","Parch"]].values
+features = train[["Pclass","Age","Sex","Fare", "Embarked","SibSp","Parch"]].values
 
 classifier = linear_model.LogisticRegression()
 classifier_ = classifier.fit(features, target)
