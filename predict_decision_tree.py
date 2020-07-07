5cefd1ed5d75d7c5289575e4b6f177d23cc02ca3
HirushaR <hirusharandunu11@gmail.com>
2020-07-07 11:09:59 +0530

    generalize tree model

--- predict_decision_tree.py
+++ predict_decision_tree.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import utils
-from sklearn import tree
+from sklearn import tree, model_selection
 
 train = pd.read_csv('data/train.csv')
 utils.clean_data(train)
@@ -13,3 +13,20 @@ decision_tree = tree.DecisionTreeClassifier(random_state=1)
 decision_tree = decision_tree.fit(features,target)
 
 print(decision_tree.score(features, target))
+
+score = model_selection.cross_val_score(decision_tree, features, target, scoring='accuracy', cv=50)
+print(score)
+print(score.mean())
+print("=====================")
+generalize_tree = tree.DecisionTreeClassifier(
+    random_state=1,
+    max_depth=7,
+    min_samples_split=2,
+)
+generalize_tree = generalize_tree.fit(features,target)
+
+print(generalize_tree.score(features, target))
+
+score = model_selection.cross_val_score(generalize_tree, features, target, scoring='accuracy', cv=50)
+print(score)
+print(score.mean())
