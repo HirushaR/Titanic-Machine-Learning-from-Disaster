73451d5bbc33ff4ce556461c6e155306cc75005a
HirushaR <hirusharandunu11@gmail.com>
2020-07-07 10:36:01 +0530

    logisteic regression 1

--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ def clean_data(data):
     data['Fare'] = data['Fare'].fillna(data['Fare'].dropna().median())
     data['Age'] = data['Age'].fillna(data['Age'].dropna().median())
 
-    data.loc[['Sex'] == "male", "Sex"] = 0
-    data.loc[['Sex'] == "female", "Sex"] = 1
+    data.loc[data['Sex'] == "male", "Sex"] = 0
+    data.loc[data['Sex'] == "female", "Sex"] = 1
 
     data["Embarked"] = data['Embarked'].fillna("S")
-    data.loc[['Embarked'] == "S", 'Embarked'] = 0
-    data.loc[['Embarked'] == "C", 'Embarked'] = 1
-    data.loc[['Embarked'] == "Q", 'Embarked'] = 2
+    data.loc[data['Embarked'] == "S", 'Embarked'] = 0
+    data.loc[data['Embarked'] == "C", 'Embarked'] = 1
+    data.loc[data['Embarked'] == "Q", 'Embarked'] = 2
