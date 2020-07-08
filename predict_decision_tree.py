0423e0b5eebcf2d04c49bbc797988ba54bff285d
HirushaR <hirusharandunu11@gmail.com>
2020-07-08 20:15:33 +0530

    export file

--- predict_decision_tree.py
+++ predict_decision_tree.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import utils
+import numpy as np
 from sklearn import tree, model_selection
 
 train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
 utils.clean_data(train)
+utils.clean_data(test)
 
 target = train["Survived"].values
 features_names = ["Pclass","Age","Sex","Fare", "Embarked","SibSp","Parch"]
@@ -13,6 +16,14 @@ decision_tree = tree.DecisionTreeClassifier(random_state=1)
 decision_tree = decision_tree.fit(features,target)
 
 print(decision_tree.score(features, target))
+
+# test_features = test[["Pclass","Age","Sex","Fare", "Embarked","SibSp","Parch"]].values
+# prdiction = decision_tree.predict(test_features)
+# PassengerId = np.array(test.PassengerId).astype(int)
+# solution = pd.DataFrame(prdiction, PassengerId, columns = ["Survived"])
+# solution.to_csv('data/decision_tree.csv', index_label = ["PassengerId"])
+
+
 
 score = model_selection.cross_val_score(decision_tree, features, target, scoring='accuracy', cv=50)
 print(score)
@@ -33,4 +44,10 @@ print(score.mean())
 
 print("===========")
 
-tree.export_graphviz(generalize_tree, feature_names=features_names, out_file="tree.dot")
+#tree.export_graphviz(generalize_tree, feature_names=features_names, out_file="tree.dot")
+
+test_features = test[["Pclass","Age","Sex","Fare", "Embarked","SibSp","Parch"]].values
+prdiction = generalize_tree.predict(test_features)
+PassengerId = np.array(test.PassengerId).astype(int)
+solution = pd.DataFrame(prdiction, PassengerId, columns = ["Survived"])
+solution.to_csv('data/decision_tree_2.csv', index_label = ["PassengerId"])
